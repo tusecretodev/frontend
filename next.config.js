bbe60f837ad1a895a8490fff2e8052824809461d
what <elmaskesuena@proton.me>
2025-08-12 19:15:31 -0300

    api

--- next.config.js
+++ next.config.js
@@ -45,7 +45,7 @@ const nextConfig = {
     formats: ['image/webp', 'image/avif'],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
-    domains: ['tusecreto.net', 'secretos.tusecreto.net', 'intranet-london-eventually-place.trycloudflare.com'],
+    domains: ['tusecreto.net', 'secretos.tusecreto.net', 'api.tusecreto.net'],
     loader: 'default',
     dangerouslyAllowSVG: false,
     contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
@@ -88,7 +88,7 @@ const nextConfig = {
           },
           {
             key: 'Content-Security-Policy',
-            value: "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://fonts.googleapis.com; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; font-src 'self' https://fonts.gstatic.com; img-src 'self' data: https:; connect-src 'self' https://intranet-london-eventually-place.trycloudflare.com; frame-ancestors 'none'; base-uri 'self'; form-action 'self'",
+            value: "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://fonts.googleapis.com; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; font-src 'self' https://fonts.gstatic.com; img-src 'self' data: https:; connect-src 'self' https://api.tusecreto.net; frame-ancestors 'none'; base-uri 'self'; form-action 'self'",
           },
           {
             key: 'Permissions-Policy',
@@ -130,7 +130,7 @@ const nextConfig = {
     return [
       {
         source: '/api/:path*',
-        destination: 'https://intranet-london-eventually-place.trycloudflare.com/api/:path*',
+        destination: 'https://api.tusecreto.net/api/:path*',
       },
     ];
   },
